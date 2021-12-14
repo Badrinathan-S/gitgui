57b0da3e7830516f3fd45a3fcc77b8ca10e7fdb8
Badrinathan-S <badrinathan3@gmail.com>
2021-12-14 17:26:53 +0530

    intial commit

---
+++ gitgui/src/main/java/gui/gitgui/App.java
@@ -0,0 +1,13 @@
+package gui.gitgui;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
